8ce169053d026e635fa3fbeb361744028f3c7a63
AFZimmers <42275473+AFZimmers@users.noreply.github.com>
2021-10-26 13:34:33 +0200

    [ADD] sw-project : project customisations
    .added new fields and tables to project
    .added button boxes

--- sw_project/models/__init__.py
+++ sw_project/models/__init__.py
@@ -4,4 +4,7 @@ from . import functional_area
 from . import related_module
 from . import task_log
 from . import bug_log
-
+from . import change_request_log
+from . import decision_log
+from . import information_log
+from . import risk_log
