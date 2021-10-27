731c98b57578c10541cdddf0237a67e85e9d607d
AFZimmers <42275473+AFZimmers@users.noreply.github.com>
2021-10-27 12:10:18 +0200

    [ADD] sw-project : project customisations
    .changed views

--- sw_project/models/task_log.py
+++ sw_project/models/task_log.py
@@ -8,11 +8,9 @@ class TaskLog(models.AbstractModel):
     name = fields.Char(string="Name")
     description = fields.Char(string="Description")
     related_task = fields.Many2one('project.task', string="Related Task", required=True)
-    logged_by = fields.Many2one('res.users',
-                                default=lambda self: self.env.user,
+    logged_by = fields.Many2one('res.partner',
                                 string='Logged By',
-                                readonly=True,
                                 required=True)
-    logged_on = fields.Date(String="Logged On")
+    logged_on = fields.Date(String="Logged On", default=fields.Date.today())
     log_source = fields.Char(string="Log Source")
     related_meeting = fields.Char(string="Related Meeting")
