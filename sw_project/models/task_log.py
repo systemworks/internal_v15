ae98057435a33784d7af76e95e8522c03d44dc94
AFZimmers <42275473+AFZimmers@users.noreply.github.com>
2021-10-28 14:33:47 +0200

    [ADD] sw-project : project customisations
    .log codes

--- sw_project/models/task_log.py
+++ sw_project/models/task_log.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class TaskLog(models.Model):
     _name = 'project.task.log'
 
-    name = fields.Char(string="Name")
+    name = fields.Char(string="Log Code")
     description = fields.Char(string="Description")
     related_task_id = fields.Many2one('project.task', string="Related Task", required=True)
     logged_by = fields.Many2one('res.partner',
@@ -20,3 +20,33 @@ class TaskLog(models.Model):
                                            ('decision_log', 'Decision Log'),
                                            ('information_log', 'Information Log')],
                                 string="Log Type")
+
+    @api.model
+    def create(self, vals):
+        res = super(TaskLog, self).create(vals)
+        if res.log_type == 'bug_log':
+            seq_no = self.env['ir.sequence'].next_by_code('project.task.log.bug')
+            res.write({
+                'name': seq_no,
+            })
+        elif res.log_type == 'change_request_log':
+            seq_no = self.env['ir.sequence'].next_by_code('project.task.log.change.request.log')
+            res.write({
+                'name': seq_no,
+            })
+        elif res.log_type == 'risk_log':
+            seq_no = self.env['ir.sequence'].next_by_code('project.task.log.risk.log')
+            res.write({
+                'name': seq_no,
+            })
+        elif res.log_type == 'decision_log':
+            seq_no = self.env['ir.sequence'].next_by_code('project.task.log.decision.log')
+            res.write({
+                'name': seq_no,
+            })
+        elif res.log_type == 'information_log':
+            seq_no = self.env['ir.sequence'].next_by_code('project.task.log.information.log')
+            res.write({
+                'name': seq_no,
+            })
+        return res
